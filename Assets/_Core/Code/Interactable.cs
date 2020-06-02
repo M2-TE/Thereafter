a63f102845f202cb47b12525ee6a8e26b289c9d9
YDenker <33430355+YDenker@users.noreply.github.com>
2020-06-02 13:57:35 +0200

    Interactables Shader

---
+++ Assets/_Core/Code/Interactable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField][Range(0.2f, 2f)] private float  interactRange = 1f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
