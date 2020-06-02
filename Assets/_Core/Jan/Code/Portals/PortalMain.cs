d166f155fa94643576e9e8e3bfed9cadec00dc95
Jan <33833587+Model2TypeE@users.noreply.github.com>
2020-06-02 12:48:55 +0200

    portal effect with proper eye seperation in vr

--- Assets/_Core/Jan/Code/Portals/PortalMain.cs
+++ Assets/_Core/Jan/Code/Portals/PortalMain.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.XR;
 
+[DefaultExecutionOrder(30000)]
 public class PortalMain : PortalBase
 {
+    [SerializeField] private float m_EyeDist = .065f;
     [SerializeField] private RenderTexture m_RtEyeLeft;
     [SerializeField] private RenderTexture m_RtEyeRight;
     [Space]
     [SerializeField] private Camera m_EyeLeft;
     [SerializeField] private Camera m_EyeRight;
-    [SerializeField] private Transform m_EyeTransformLeft;
-    [SerializeField] private Transform m_EyeTransformRight;
     private Camera m_MainCam;
+
+    // testing
+
+    //
 
     [Header("Cam Stuff - TODO")]
     protected int parameterHashVector;
@@ -21,45 +25,76 @@ public class PortalMain : PortalBase
     public Vector4 rightEye = new Vector4(0.0f, 0.5f, 1.0f, 0.5f);
     bool m_RightEyeQueued = false;
 
+    private void OnEnable()
+    {
+        RenderPipelineManager.beginCameraRendering += DoStuff;
+        Application.onBeforeRender += OnBeforeRender;
+    }
+
+    private void OnDisable()
+    {
+        RenderPipelineManager.beginCameraRendering -= DoStuff;
+        Application.onBeforeRender -= OnBeforeRender;
+    }
+
     private void Start()
     {
         parameterHashVector = Shader.PropertyToID("_EyeTransformVector");
         parameterHashFloat = Shader.PropertyToID("_EyeFloatFlag");
         m_MainCam = Camera.main;
-        RenderPipelineManager.beginCameraRendering += DoStuff;
     }
 
-
-    private void Update()
+    private void OnBeforeRender()
     {
+        RenderLeft();
+        RenderRight();
+
+        //m_MainCam.Render();
+    }
+
+    private void RenderLeft(bool adjustPos = true)
+    {
+        if(adjustPos)
         {
-            m_EyeLeft.projectionMatrix = m_MainCam.projectionMatrix; // Match matrices
-            m_EyeRight.projectionMatrix = m_MainCam.projectionMatrix; // Match matrices
+            m_EyeLeft.projectionMatrix = m_MainCam.projectionMatrix;
 
-            // left eye
-            {
-                var relativePosition = transform.InverseTransformPoint(m_EyeTransformLeft.position);
-                relativePosition = Vector3.Scale(relativePosition, new Vector3(-1, 1, -1));
-                m_EyeLeft.transform.position = m_Pair.transform.TransformPoint(relativePosition);
+            var relativePosition = transform.InverseTransformPoint(m_MainCam.transform.position);
+            relativePosition = Vector3.Scale(relativePosition, new Vector3(-1, 1, -1));
+            m_EyeLeft.transform.position = m_Pair.transform.TransformPoint(relativePosition);
 
 
-                var relativeRotation = transform.InverseTransformDirection(m_MainCam.transform.forward);
-                relativeRotation = Vector3.Scale(relativeRotation, new Vector3(-1, 1, -1));
-                m_EyeLeft.transform.forward = m_Pair.transform.TransformDirection(relativeRotation);
-            }
+            var relativeRotation = transform.InverseTransformDirection(m_MainCam.transform.forward);
+            relativeRotation = Vector3.Scale(relativeRotation, new Vector3(-1, 1, -1));
+            m_EyeLeft.transform.forward = m_Pair.transform.TransformDirection(relativeRotation);
 
-            // right eye
-            {
-                var relativePosition = transform.InverseTransformPoint(m_EyeTransformRight.position);
-                relativePosition = Vector3.Scale(relativePosition, new Vector3(-1, 1, -1));
-                m_EyeRight.transform.position = m_Pair.transform.TransformPoint(relativePosition);
-
-
-                var relativeRotation = transform.InverseTransformDirection(m_MainCam.transform.forward);
-                relativeRotation = Vector3.Scale(relativeRotation, new Vector3(-1, 1, -1));
-                m_EyeRight.transform.forward = m_Pair.transform.TransformDirection(relativeRotation);
-            }
+            m_EyeLeft.transform.Rotate(new Vector3(0f, 0f, m_MainCam.transform.rotation.eulerAngles.z));
+            m_EyeLeft.transform.Translate(new Vector3(-m_EyeDist * .5f, 0f, 0f), Space.Self);
         }
+
+        SetActiveShaderEye(Camera.MonoOrStereoscopicEye.Left);
+        m_EyeLeft.Render();
+    }
+
+    private void RenderRight(bool adjustPos = true)
+    {
+        if (adjustPos)
+        {
+            m_EyeRight.projectionMatrix = m_MainCam.GetStereoNonJitteredProjectionMatrix(Camera.StereoscopicEye.Right);
+
+            var relativePosition = transform.InverseTransformPoint(m_MainCam.transform.position);
+            relativePosition = Vector3.Scale(relativePosition, new Vector3(-1, 1, -1));
+            m_EyeRight.transform.position = m_Pair.transform.TransformPoint(relativePosition);
+
+            var relativeRotation = transform.InverseTransformDirection(m_MainCam.transform.forward);
+            relativeRotation = Vector3.Scale(relativeRotation, new Vector3(-1, 1, -1));
+            m_EyeRight.transform.forward = m_Pair.transform.TransformDirection(relativeRotation);
+
+            m_EyeRight.transform.Rotate(new Vector3(0f, 0f, m_MainCam.transform.rotation.eulerAngles.z));
+            m_EyeRight.transform.Translate(new Vector3(m_EyeDist * .5f, 0f, 0f), Space.Self);
+        }
+
+        SetActiveShaderEye(Camera.MonoOrStereoscopicEye.Right);
+        m_EyeRight.Render();
     }
 
     private void DoStuff(ScriptableRenderContext ctx, Camera cam)
