85fb38b080adfd8f6c82d7919f4147765c787799
YDenker <33430355+YDenker@users.noreply.github.com>
2020-06-04 21:56:43 +0200

    fixed most shaders on inported objects and attached robohands to player (not testet in vr)

---
+++ Assets/_Core/Yannick/Code/HandAnimationTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimationTrigger : MonoBehaviour
+{
+    public Animator LeftHand, RightHand;
+
+    [SerializeField]
+    private bool isOpen = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) LeftHand.SetBool("isOpen", false);
+        if(Input.GetKeyDown(KeyCode.E)) RightHand.SetBool("isOpen", false);
+        if(Input.GetKeyUp(KeyCode.E)) RightHand.SetBool("isOpen", true);
+        if (Input.GetKeyUp(KeyCode.Space)) LeftHand.SetBool("isOpen", true);
+    }
+}
