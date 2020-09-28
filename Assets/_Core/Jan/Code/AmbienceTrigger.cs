68dd0c827b25c97ec27d054ef878161b644d7387
Jan <33833587+Model2TypeE@users.noreply.github.com>
2020-09-28 20:43:13 +0200

    in progress audio implementation

---
+++ Assets/_Core/Jan/Code/AmbienceTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AmbienceTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioManager m_Manager;
+    [SerializeField] private AudioClip m_EntryRoomAmbience;
+    [SerializeField] private float m_Vol = 1.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_Manager.PlayAmbience(m_EntryRoomAmbience, m_Vol);
+    }
+}
---
+++ Assets/_Core/Jan/Code/AudioManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource m_Player;
+
+    public void PlayAmbience(AudioClip clip, float vol)
+    {
+        m_Player.PlayOneShot(clip, vol);
+    }
+}
